68ae7bdb30466ed2cdca2072f8d3f5a123849f5c
Esteban Melo <hawk1809em@gmail.com>
2026-08-26 20:06:43 -0500

    Mejora diseño visual de login en Android

--- mobile-app/AILearningMobile/app/build.gradle.kts
+++ mobile-app/AILearningMobile/app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
